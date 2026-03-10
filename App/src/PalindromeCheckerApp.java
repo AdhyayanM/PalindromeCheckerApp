ef3069a30a4392c3e211d877fc6f6d77293459ef
AdhyayanM <adhyayanmalguri27@gmail.com>
2026-03-10 10:28:23 +0530

    UC3-UsinArray

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,24 +1,21 @@
 import java.util.Scanner;
-public class PalindroneCheckerApp {
+public class PalindromeCheckerApp {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to Palindrome Checker App Management System");
- HEAD
+
         System.out.println("Enter string to check: ");
         String input = scanner.nextLine();
 
-        String input = "madam";
- baf42b4a3c073b9c614adb5fc8ea3ee1a4469de8
         String front = "";
         for (int i = 0; i < input.length(); i++) {
             front += input.charAt(i); // add each character
         }
         System.out.println("Front collected: " + front);
- HEAD
 
 
 
- baf42b4a3c073b9c614adb5fc8ea3ee1a4469de8
+
         String reverse = "";
         for (int i = input.length() - 1; i >= 0; i--) {
             reverse += input.charAt(i);
