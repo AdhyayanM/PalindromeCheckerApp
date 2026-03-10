1d4cc274be1a09a41dac628f49e6bf3baced4a5c
AdhyayanM <adhyayanmalguri27@gmail.com>
2026-03-10 10:31:11 +0530

    UC4-CharacterArray

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -2,33 +2,19 @@ import java.util.Scanner;
 public class PalindroneCheckerApp {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome to Palindrome Checker App Management System");
- HEAD
-        System.out.println("Enter string to check: ");
-        String input = scanner.nextLine();
-
-        String input = "madam";
- baf42b4a3c073b9c614adb5fc8ea3ee1a4469de8
-        String front = "";
-        for (int i = 0; i < input.length(); i++) {
-            front += input.charAt(i); // add each character
+        String input = "radar";
+        char[] chars = input.toCharArray();
+        int start = 0;
+        int end = chars.length - 1;
+        boolean isPalindrome = false;
+        while(start<end){
+            if(chars[start] == chars[end]){
+                isPalindrome = true;
+            }
+            start++;
+            end--;
         }
-        System.out.println("Front collected: " + front);
- HEAD
-
-
-
- baf42b4a3c073b9c614adb5fc8ea3ee1a4469de8
-        String reverse = "";
-        for (int i = input.length() - 1; i >= 0; i--) {
-            reverse += input.charAt(i);
-        }
-
-        System.out.println("Reverse collected: " +reverse);
-        if (reverse.equals(front)) {
-            System.out.println("Palindrome");}
-        else
-            System.out.println("Not Palindrome");
-
+        System.out.println("Input: " + input);
+        System.out.println("is Palindrome? " + isPalindrome);
     }
 }
