70cd65f361e467c2f97d12caf2eb3cfa37c1e6f4
AdhyayanM <adhyayanmalguri27@gmail.com>
2026-03-25 11:13:53 +0530

    UC5-StackBased

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,48 +1,24 @@
-import java.util.Scanner;
+import java.util.Stack;
+
 public class PalindromeCheckerApp {
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-<<<<<<< HEAD
-        String input = "radar";
-        char[] chars = input.toCharArray();
-        int start = 0;
-        int end = chars.length - 1;
-        boolean isPalindrome = false;
-        while(start<end){
-            if(chars[start] == chars[end]){
-                isPalindrome = true;
+    public static void main(String[] args) {
+        String input = "noon";
+        Stack<Character> stack = new Stack<>();
+        for (char c : input.toCharArray()) {
+            stack.push(c);
+        }
+        boolean isPalindrome = true;
+        for (char c : input.toCharArray()) {
+            char poppedChar = stack.pop();
+            if (c != poppedChar) {
+                isPalindrome = false;
+                break;
             }
-            start++;
-            end--;
         }
-        System.out.println("Input: " + input);
-        System.out.println("is Palindrome? " + isPalindrome);
-=======
-        System.out.println("Welcome to Palindrome Checker App Management System");
-
-        System.out.println("Enter string to check: ");
-        String input = scanner.nextLine();
-
-        String front = "";
-        for (int i = 0; i < input.length(); i++) {
-            front += input.charAt(i); // add each character
+        if (isPalindrome) {
+            System.out.println(input + " is a palindrome.");
+        } else {
+            System.out.println(input + " is not a palindrome.");
         }
-        System.out.println("Front collected: " + front);
-
-
-
-
-        String reverse = "";
-        for (int i = input.length() - 1; i >= 0; i--) {
-            reverse += input.charAt(i);
-        }
-
-        System.out.println("Reverse collected: " +reverse);
-        if (reverse.equals(front)) {
-            System.out.println("Palindrome");}
-        else
-            System.out.println("Not Palindrome");
-
->>>>>>> 940ac01a22a0d8d526b10e09ad24c86afed41c6c
     }
 }
